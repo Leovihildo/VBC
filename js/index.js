e91a0982fdcacff941f333ce4dab477a2245e788
Leovihildo <46786869+Leovihildo@users.noreply.github.com>
2023-03-17 07:34:41 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -45,18 +45,16 @@ document.querySelector(".continue").addEventListener('click', () => {
 })
 
 // Swiper Container
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    }
-})
+var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
